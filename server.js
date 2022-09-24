a3c71dab77a82f293f293e044304c67dee8b0673
Andrew Chang <acchang4@gmail.com>
2022-09-23 20:43:18 -0400

    working through to 41

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 8000
+require('dotenv').config
 
-app.get('/', )
+const wisecracks = {
+    'quote': 'ouch',
+    'year': 1000,
+    'name': 'jesus'
+}
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (req, res) => {
+    res.json(wisecracks)
+})
+
+app.get('/api/:name', (req, res)=>{
+    console.log(req.params.name)
+    res.json(wisecracks)
+})
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`Server running on ${PORT}. Go catch it!`)
+})
+
