2ace07b029a6a2ac9841229cfda7a03c9776a542
Andrew Chang <acchang4@gmail.com>
2022-09-25 12:31:43 -0400

    working version

--- server.js
+++ server.js
@@ -1,39 +1,63 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 8000
-require('dotenv').config
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env" });
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }).then(
-    (client) => {
-        console.log(`Connected to ${dbName} Database`);
-        db = client.db(dbName);
-    }
-    );
+// Passport config
+require("./config/passport")(passport);
 
+//Connect To Database
+connectDB();
+
+//Using EJS for views
 app.set("view engine", "ejs");
+
+//Static Folder
 app.use(express.static("public"));
+
+//Body Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html')
-})
+//Logging
+app.use(logger("dev"));
 
-app.get('/api', (req, res) => {
-    res.json(wisecracks)
-})
+//Use forms for put / delete
+app.use(methodOverride("_method"));
 
-app.get('/api/:name', (req, res)=>{
-    console.log(req.params.name)
-    res.json(wisecracks)
-})
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
-app.listen(process.env.PORT || PORT, () =>{
-    console.log(`Server running on ${PORT}. Go catch it!`)
-})
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
+//Use flash messages for errors, info, ect...
+app.use(flash());
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
